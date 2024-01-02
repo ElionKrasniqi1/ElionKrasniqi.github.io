a4075a2959be3a2d3258e3cf6afd516f843a9018
ElionKrasniqi1 <155365695+ElionKrasniqi1@users.noreply.github.com>
2024-01-02 13:37:17 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+console.log('Përshëndetje dhe mirësevini në Dyqanin tim personal!');
+
+function hello() {
+    console.log('Hello!');
+    console.log('Welcome to JavaScript');
+}hello
+
+function ndryshoUrl() {
+    var dropdown = document.getElementById("Shoes");
+    var url = dropdown.value;
+    window.location.href = url;
+}
